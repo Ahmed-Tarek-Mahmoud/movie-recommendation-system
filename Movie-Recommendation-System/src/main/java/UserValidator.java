1728a74c12da67fca1138fa2d5bbb9c094af9922
Ahmed-nasr129 <ahmednasreldeen129@gmail.com>
2025-11-25 23:15:16 +0200

    implement user id validator and add unit testcases for it

--- Movie-Recommendation-System/src/main/java/UserValidator.java
+++ Movie-Recommendation-System/src/main/java/UserValidator.java
@@ -2,6 +2,7 @@ public interface UserValidator {
 
     default void ValidUser(User user){
         UserNameValid(user.getUserName());
+        UserIdValid(user.getUserId());
     }
 
     default void UserNameValid(String userName){
@@ -22,6 +23,18 @@ public interface UserValidator {
             if (!Character.isLetter(c) && c != ' ') {
                 throw new AppExceptions("User name must contain only alphabetic characters and spaces", ErrorCode.USER_NAME_ERROR);
             }
+        }
+    }
+    default void UserIdValid(String userId){
+        if (userId == null) {
+            throw new NullPointerException("User id cannot be null");
+        }
+        if(userId.length() != 9){
+            throw new AppExceptions("User id length must equal 9", ErrorCode.USER_ID_LENGTH_ERROR);
+        }
+        for(int i=0;i<userId.length();i++){
+            if( (i==8 && !Character.isLetterOrDigit(userId.charAt(i)) ) || (i!=8 && !Character.isDigit(userId.charAt(i))))
+                throw new AppExceptions("User id must contain only digits and might end with char", ErrorCode.USER_ID_ERROR);
         }
     }
 }
